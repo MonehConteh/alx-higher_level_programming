5a5a3fec1776387b6b42566ff04622c78736a773
MonehConteh <moneh40824@gmail.com>
2022-09-25 16:41:49 +0000

    a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -18,4 +18,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
     sum_tup = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return 
+    return sum_tup
