23e1bf6ff7d252c47ea7e21c66fd03bc16579ead
MonehConteh <Moneh40824@gmail.com>
2023-08-15 16:21:16 -0700

    List header files

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -2,5 +2,6 @@
 #define LISTS_HEADER_FILE
 def print_list_integer(my_list=[]):
 def element_at(my_list, idx):
-
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p);
 #endif
